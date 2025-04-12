bee7e8de31eb11516ed29451a6a942105b31a38c
pieroyany <yanypiero@gmail.com>
2025-04-12 00:49:02 -0400

    funcion registrar cliente

--- tarea1.c
+++ tarea1.c
@@ -7,31 +7,41 @@
 void mostrarMenuPrincipal() {
   limpiarPantalla();
   puts("========================================");
-  puts("     Sistema de Gestión Hospitalaria");
+  puts("     Sistema de Servivio Técnico");
   puts("========================================");
 
-  puts("1) Registrar paciente");
+  puts("1) Registrar cliente");
   puts("2) Asignar prioridad a paciente");
   puts("3) Mostrar lista de espera");
   puts("4) Atender al siguiente paciente");
-  puts("5) Mostrar pacientes por prioridad");
+  puts("5) Mostrar clientes por prioridad");
   puts("6) Salir");
 }
 
-void registrar_paciente(List *pacientes) {
-  printf("Registrar nuevo paciente\n");
-  // Aquí implementarías la lógica para registrar un nuevo paciente
+void registrar_cliente(List *clientes) {
+  char nombre_cliente[50];
+  char dispositivo;
+  char fecha[10];
+
+  printf("Registrar nuevo cliente\n");
+  printf("Ingrese el nombre del cliente:\n");
+  scanf("%s", nombre_cliente);
+  printf("Ingrese el tipo de dispositivo\n1: smartphone\n2: laptop\n3: audífonos\n");
+  scanf(" %c", &dispositivo);
+  printf("Ingrese la fecha de ingreso (DD/MM/AAAA):\n");
+  scanf("%s", fecha);
+  
 }
 
-void mostrar_lista_pacientes(List *pacientes) {
-  // Mostrar pacientes en la cola de espera
-  printf("Pacientes en espera: \n");
-  // Aquí implementarías la lógica para recorrer y mostrar los pacientes
+void mostrar_lista_cliente(List *clientes) {
+  // Mostrar clientes en la cola de espera
+  printf("Clientes en espera: \n");
+  // Aquí implementarías la lógica para recorrer y mostrar los clientes
 }
 
 int main() {
   char opcion;
-  List *pacientes = list_create(); // puedes usar una lista para gestionar los pacientes
+  List *clientes = list_create(); // puedes usar una lista para gestionar los clientes
 
   do {
     mostrarMenuPrincipal();
@@ -41,22 +51,22 @@ int main() {
 
     switch (opcion) {
     case '1':
-      registrar_paciente(pacientes);
+      registrar_cliente(clientes);
       break;
     case '2':
       // Lógica para asignar prioridad
       break;
     case '3':
-      mostrar_lista_pacientes(pacientes);
+      mostrar_lista_clientes(clientes);
       break;
     case '4':
-      // Lógica para atender al siguiente paciente
+      // Lógica para atender al siguiente clientes
       break;
     case '5':
-      // Lógica para mostrar pacientes por prioridad
+      // Lógica para mostrar clientes por prioridad
       break;
     case '6':
-      puts("Saliendo del sistema de gestión hospitalaria...");
+      puts("Saliendo del sistema de servicio técnico...");
       break;
     default:
       puts("Opción no válida. Por favor, intente de nuevo.");
@@ -66,7 +76,7 @@ int main() {
   } while (opcion != '6');
 
   // Liberar recursos, si es necesario
-  list_clean(pacientes);
+  list_clean(clientes);
 
   return 0;
 }
