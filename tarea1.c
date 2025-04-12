740aa1a9cf86785f6097eefd911095d666021791
pieroyany <yanypiero@gmail.com>
2025-04-12 01:40:02 -0400

    lower_than y sortedinsert

--- tarea1.c
+++ tarea1.c
@@ -2,7 +2,7 @@
 #include "tdas/extra.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+//------------------------------------------------------------------------------------------------
 typedef struct {
   char nombre[50];
   char dispositivo;
@@ -15,7 +15,30 @@ typedef struct {
   char mes[2];
   char año[4];
 } fecha;
+//------------------------------------------------------------------------------------------------
+// funcion para usar sortedinsert
+int lower_than(char *data1, char *data2) {
+  Cliente *a = (Cliente *)data1;
+  Cliente *b = (Cliente *)data2;
 
+  // Comparar prioridad
+  if (a->prioridad != b->prioridad)
+      return a->prioridad > b->prioridad;
+
+  // misma prioridad, comparar fecha
+  int anioA = atoi(a->fecha.año);
+  int anioB = atoi(b->fecha.año);
+  if (anioA != anioB) return anioA < anioB;
+
+  int mesA = atoi(a->fecha.mes);
+  int mesB = atoi(b->fecha.mes);
+  if (mesA != mesB) return mesA < mesB;
+
+  int diaA = atoi(a->fecha.dia);
+  int diaB = atoi(b->fecha.dia);
+  return diaA < diaB;
+}
+//------------------------------------------------------------------------------------------------
 // Menú principal
 void mostrarMenuPrincipal() {
   limpiarPantalla();
@@ -31,19 +54,26 @@ void mostrarMenuPrincipal() {
 }
 
 void registrar_cliente(List *clientes) {
-  Cliente nuevo_cliente;
+  Cliente *nuevo_cliente = (Cliente *)malloc(sizeof(Cliente));
+  if (nuevo_cliente == NULL) {
+    printf("Error al asignar memoria\n");
+    return;
+  }
+
   printf("Registrar nuevo cliente\n");
   printf("Ingrese el nombre del cliente:\n");
-  scanf("%s", &nuevo_cliente.nombre);
+  scanf("%s", &nuevo_cliente->nombre);
 
   printf("Ingrese el tipo de dispositivo\n1: smartphone\n2: laptop\n3: audífonos\n");
-  scanf(" %c", &nuevo_cliente.dispositivo);
+  scanf(" %c", &nuevo_cliente->dispositivo);
 
   printf("Ingrese la fecha de ingreso (DD/MM/AAAA):\n");
-  scanf("%s/%s/%s", &nuevo_cliente.fecha->dia, &nuevo_cliente.fecha->mes, &nuevo_cliente.fecha.año);
+  scanf("%s/%s/%s", &nuevo_cliente->fecha.dia, &nuevo_cliente->fecha.mes, &nuevo_cliente->fecha.año);
   
   printf("Ingrese la prioridad de atención del cliente (1-3):\n");
-  scanf(" %c", &nuevo_cliente.prioridad);
+  scanf(" %c", &nuevo_cliente->prioridad);
+
+  list_sortedInsert(clientes, nuevo_cliente, lower_than);
 }
 
 void mostrar_lista_cliente(List *clientes) {
