0ab519899f9f5518e8a556046cc4235dbef1511b
pieroyany <yanypiero@gmail.com>
2025-04-12 02:02:47 -0400

    mostrar lista de clientes

--- tarea1.c
+++ tarea1.c
@@ -49,17 +49,17 @@ void mostrarMenuPrincipal() {
   puts("1) Registrar cliente");
   puts("2) Mostrar lista de espera");
   puts("3) Atender al siguiente paciente");
-  puts("4) Mostrar clientes por prioridad");
-  puts("5) Salir");
+  puts("4) Salir");
 }
 
 void registrar_cliente(List *clientes) {
+  //reservar memoria para el nuevo cliente
   Cliente *nuevo_cliente = (Cliente *)malloc(sizeof(Cliente));
   if (nuevo_cliente == NULL) {
     printf("Error al asignar memoria\n");
     return;
   }
-
+  //rellenar los datos del cliente
   printf("Registrar nuevo cliente\n");
   printf("Ingrese el nombre del cliente:\n");
   scanf("%s", &nuevo_cliente->nombre);
@@ -73,13 +73,18 @@ void registrar_cliente(List *clientes) {
   printf("Ingrese la prioridad de atención del cliente (1-3):\n");
   scanf(" %c", &nuevo_cliente->prioridad);
 
+  //una vez rellenados los datos, se inserta el cliente en la lista
   list_sortedInsert(clientes, nuevo_cliente, lower_than);
 }
 
 void mostrar_lista_cliente(List *clientes) {
   // Mostrar clientes en la cola de espera
   printf("Clientes en espera: \n");
-  // Aquí implementarías la lógica para recorrer y mostrar los clientes
+  for (int i = 0; i < list_size(clientes); i++) {
+    Cliente *cliente = (Cliente *)list_first(clientes);
+    printf("Cliente %d: %s, Dispositivo: %c, Fecha: %s/%s/%s, Prioridad: %c",i + 1, cliente->nombre, cliente->dispositivo, cliente->fecha.dia, cliente->fecha.mes, cliente->fecha.año, cliente->prioridad);
+    list_next(clientes);
+  }
 }
 
 int main() {
@@ -100,12 +105,9 @@ int main() {
       mostrar_lista_clientes(clientes);
       break;
     case '3':
-      // Lógica para atender al siguiente clientes
+      // Lógica para atender al siguiente cliente
       break;
     case '4':
-      // Lógica para mostrar clientes por prioridad
-      break;
-    case '5':
       puts("Saliendo del sistema de servicio técnico...");
       break;
     default:
@@ -113,7 +115,7 @@ int main() {
     }
     presioneTeclaParaContinuar();
 
-  } while (opcion != '6');
+  } while (opcion != '4');
 
   // Liberar recursos, si es necesario
   list_clean(clientes);
