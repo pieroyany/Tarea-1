717b0e57ab79aff0e65c8d79c249bdadb8997eba
pieroyany <yanypiero@gmail.com>
2025-04-12 01:02:15 -0400

    funcion registrar_cliente

--- tarea1.c
+++ tarea1.c
@@ -3,6 +3,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct {
+  char nombre[50];
+  char dispositivo;
+  fecha fecha;
+  char prioridad;
+} Cliente;
+
+typedef struct {
+  char dia[2];
+  char mes[2];
+  char año[4];
+} fecha;
+
 // Menú principal
 void mostrarMenuPrincipal() {
   limpiarPantalla();
@@ -11,26 +24,26 @@ void mostrarMenuPrincipal() {
   puts("========================================");
 
   puts("1) Registrar cliente");
-  puts("2) Asignar prioridad a paciente");
-  puts("3) Mostrar lista de espera");
-  puts("4) Atender al siguiente paciente");
-  puts("5) Mostrar clientes por prioridad");
-  puts("6) Salir");
+  puts("2) Mostrar lista de espera");
+  puts("3) Atender al siguiente paciente");
+  puts("4) Mostrar clientes por prioridad");
+  puts("5) Salir");
 }
 
 void registrar_cliente(List *clientes) {
-  char nombre_cliente[50];
-  char dispositivo;
-  char fecha[10];
-
+  Cliente nuevo_cliente;
   printf("Registrar nuevo cliente\n");
   printf("Ingrese el nombre del cliente:\n");
-  scanf("%s", nombre_cliente);
+  scanf("%s", &nuevo_cliente->nombre);
+
   printf("Ingrese el tipo de dispositivo\n1: smartphone\n2: laptop\n3: audífonos\n");
-  scanf(" %c", &dispositivo);
+  scanf(" %c", &nuevo_cliente->dispositivo);
+
   printf("Ingrese la fecha de ingreso (DD/MM/AAAA):\n");
-  scanf("%s", fecha);
+  scanf("%s/%s/%s", &nuevo_cliente->fecha->dia, &nuevo_cliente->fecha->mes, &nuevo_cliente->fecha->año);
   
+  printf("Ingrese la prioridad de atención del cliente (1-3):\n");
+  scanf(" %c", &nuevo_cliente->prioridad);
 }
 
 void mostrar_lista_cliente(List *clientes) {
@@ -54,18 +67,15 @@ int main() {
       registrar_cliente(clientes);
       break;
     case '2':
-      // Lógica para asignar prioridad
-      break;
-    case '3':
       mostrar_lista_clientes(clientes);
       break;
-    case '4':
+    case '3':
       // Lógica para atender al siguiente clientes
       break;
-    case '5':
+    case '4':
       // Lógica para mostrar clientes por prioridad
       break;
-    case '6':
+    case '5':
       puts("Saliendo del sistema de servicio técnico...");
       break;
     default:
