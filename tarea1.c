278ae92c09b7009ac7181e0d073f91713da62d52
pieroyany <yanypiero@gmail.com>
2025-04-13 01:40:08 -0400

    detalles finales y comentarios

--- tarea1.c
+++ tarea1.c
@@ -4,9 +4,9 @@
 #include <stdlib.h>
 //------------------------------------------------------------------------------------------------
 typedef struct {
-  char dia[2];
-  char mes[2];
-  char anio[4];
+  char dia[3];
+  char mes[3];
+  char anio[5];
 } fecha;
 
 typedef struct {
@@ -43,7 +43,7 @@ int lower_than(void *data1, void *data2) {
 void mostrarMenuPrincipal() {
   limpiarPantalla();
   puts("========================================");
-  puts("     Sistema de Servivio Técnico");
+  puts("     Sistema de Servivio Tecnico");
   puts("========================================");
 
   puts("1) Registrar cliente");
@@ -59,31 +59,72 @@ void registrar_cliente(List *clientes) {
     printf("Error al asignar memoria\n");
     return;
   }
+  strcpy(nuevo_cliente->fecha.anio, "2025"); // año fijo para simplificar
   //rellenar los datos del cliente
   printf("Registrar nuevo cliente\n");
   printf("Ingrese el nombre del cliente:\n");
   scanf("%s", &nuevo_cliente->nombre);
 
-  printf("Ingrese el tipo de dispositivo\n1: smartphone\n2: laptop\n3: audífonos\n");
-  scanf(" %c", &nuevo_cliente->dispositivo);
-
-  printf("Ingrese la fecha de ingreso (DD/MM/AAAA):\n");
-  scanf("%s/%s/%s", &nuevo_cliente->fecha.dia, &nuevo_cliente->fecha.mes, &nuevo_cliente->fecha.anio);
+  char temp; // variable temporal para asignar el dispositivo comodamente
+  printf("Ingrese el tipo de dispositivo\n1: smartphone\n2: laptop\n3: audifonos\n4: otro\n");
+  scanf(" %c", &temp);
+  if (temp == '1') {
+    nuevo_cliente->dispositivo = 'smartphone';
+  } else if (temp == '2') {
+    nuevo_cliente->dispositivo = 'laptop';
+  } else if (temp == '3') {
+    nuevo_cliente->dispositivo = 'audifonos';
+  } else if (temp == '4') {
+    nuevo_cliente->dispositivo = 'otro';
+  } else {
+    printf("Dispositivo no valido\n");
+    free(nuevo_cliente);
+    return;
+  }
+  // input individual para dia y mes para mayor comodidad del usuario
+  printf("Ingrese el dia de ingreso (01 - 31):\n");
+  scanf("%s", &nuevo_cliente->fecha.dia);
+  if (atoi(nuevo_cliente->fecha.dia) < 1 || atoi(nuevo_cliente->fecha.dia) > 31) {
+    printf("Dia no valido\n");
+    free(nuevo_cliente);
+    return;
+  }
+  printf("Ingrese el mes de ingreso (01 - 12):\n");
+  scanf("%s", &nuevo_cliente->fecha.mes);
+  if (atoi(nuevo_cliente->fecha.mes) < 1 || atoi(nuevo_cliente->fecha.mes) > 12) {
+    printf("Mes no valido\n");
+    free(nuevo_cliente);
+    return;
+  }
   
   printf("Ingrese la prioridad de atención del cliente (1-3):\n");
   scanf(" %c", &nuevo_cliente->prioridad);
-
-  //una vez rellenados los datos, se inserta el cliente en la lista
+  if (nuevo_cliente->prioridad != '1' && nuevo_cliente->prioridad != '2' && nuevo_cliente->prioridad != '3') {
+    printf("Prioridad no valida\n");
+    free(nuevo_cliente);
+    return;
+  }
+  //una vez rellenados los datos, se inserta el cliente en la lista, ordenado primero por prioridad y despues por fecha
   list_sortedInsert(clientes, nuevo_cliente, lower_than);
 }
 
 void mostrar_lista_clientes(List *clientes) {
+  // Comprobar si la lista está vacía
+  if (list_size(clientes) == 0) {
+    puts("No hay clientes en espera.");
+    return;
+  }
   // Mostrar clientes en la cola de espera
   printf("Clientes en espera: \n");
-  for (int i = 0; i < list_size(clientes); i++) {
-    Cliente *cliente = (Cliente *)list_first(clientes);
-    printf("Cliente %d: %s, Dispositivo: %c, Fecha: %s/%s/%s, Prioridad: %c",i + 1, cliente->nombre, cliente->dispositivo, cliente->fecha.dia, cliente->fecha.mes, cliente->fecha.anio, cliente->prioridad);
-    list_next(clientes);
+  Cliente *cliente = (Cliente *)list_first(clientes);
+  int i = 1;
+  while (cliente != NULL) {
+    printf("Cliente %d: %s, Dispositivo: %c, Fecha: %s/%s/%s, Prioridad: %c\n",
+           i, cliente->nombre, cliente->dispositivo,
+           cliente->fecha.dia, cliente->fecha.mes, cliente->fecha.anio,
+           cliente->prioridad);
+    cliente = (Cliente *)list_next(clientes); // avanzamos al siguiente
+    i++;
   }
 }
 
@@ -93,38 +134,35 @@ int main() {
 
   do {
     mostrarMenuPrincipal();
-    printf("Ingrese su opción: ");
+    printf("Ingrese su opcion: ");
     scanf(" %c", &opcion);
 
     switch (opcion) {
     case '1':
       registrar_cliente(clientes);
-      presioneTeclaParaContinuar();
-      limpiarPantalla();
+      
       break;
+
     case '2':
       mostrar_lista_clientes(clientes);
-      presioneTeclaParaContinuar();
-      limpiarPantalla();
+      
       break;
+
     case '3':
       if (list_size(clientes) > 0) {
         Cliente *cliente_atendido = (Cliente *)list_popFront(clientes);
         printf("Atendiendo a: %s, Dispositivo: %c, Fecha: %s/%s/%s, Prioridad: %c\n", cliente_atendido->nombre, cliente_atendido->dispositivo, cliente_atendido->fecha.dia, cliente_atendido->fecha.mes, cliente_atendido->fecha.anio, cliente_atendido->prioridad);
         free(cliente_atendido); // liberar memoria del cliente atendido
-        presioneTeclaParaContinuar();
-        limpiarPantalla();
+
       } else {
         puts("No hay clientes en espera.");
-        presioneTeclaParaContinuar();
-        limpiarPantalla();
       }
       break;
     case '4':
-      puts("Saliendo del sistema de servicio técnico...");
+      puts("Saliendo del sistema de servicio tecnico...");
       break;
     default:
-      puts("Opción no válida. Por favor, intente de nuevo.");
+      puts("Opción no valida. Por favor, intente de nuevo.");
     }
     presioneTeclaParaContinuar();
 
